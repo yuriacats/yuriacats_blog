4f4ec09671eb07bba30fe44e886a8c3ad60364aa
yuria <yuria.soft0615@gmail.com>
2021-05-16 05:32:04 +0900

    Edit manifest.json

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import {NavLink} from "react-router-dom";
 const Footer = () =>(
     <footer>
+        <NavLink to={"/edit"} activeStyle={{ textDecoration: 'none' }} >
         <p>©2021- 上原ゆりあ</p>
+        </NavLink>
     </footer>
 )
 
